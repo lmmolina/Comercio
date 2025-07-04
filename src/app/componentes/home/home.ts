706454b143858c682f4f52ae53a9be59e20d8d84
Luis Miguel <lmmolinabe92@gmail.com>
2025-07-04 13:50:56 +0200

    carrusel

--- src/app/componentes/home/home.ts
+++ src/app/componentes/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { Productos } from '../../servicios/productos';
+import { Producto } from '../../modelos/producto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgbCarouselModule, CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
 export class Home {
-
+  productos!: Producto[];
+  constructor(productoService: Productos) {
+    this.productos = productoService.productos;
+  }
 }
